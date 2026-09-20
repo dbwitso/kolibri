100a1b0f69330ceb637877c37e1dabc99bf64a16
Dabwitso Mweemba <mweembadabwitso@gmail.com>
2026-09-20 14:48:53 +0200

    Show 'Locked by your coach' state on hybrid lesson cards

--- kolibri/plugins/edulution/assets/src/views/HybridLearningLessonCard.vue
+++ kolibri/plugins/edulution/assets/src/views/HybridLearningLessonCard.vue
@@ -1,7 +1,33 @@
 <template>
 
   <div class="card drop-shadow">
+    <div
+      v-if="locked"
+      class="card card-content locked-card"
+      :class="{ 'mobile-card': isMobile }"
+      :style="{ backgroundColor: $themeTokens.surface }"
+    >
+      <div class="thumbnail">
+        <CardThumbnail
+          :isMobile="isMobile"
+          :contentNode="content"
+          :hideDuration="true"
+        />
+      </div>
+      <h3 class="title">
+        <TextTruncator
+          :text="content.title"
+          :maxHeight="maxTitleHeight"
+          :style="{ color: $themeTokens.annotation }"
+        />
+      </h3>
+      <div class="locked-label" :style="{ color: $themeTokens.annotation }">
+        <KIcon icon="warning" :style="{ fill: $themePalette.orange.v_400 }" />
+        {{ $tr('lockedByCoachLabel') }}
+      </div>
+    </div>
     <router-link
+      v-else
       :to="link"
       class="card card-content"
       :class="[
@@ -71,10 +97,20 @@
         type: Boolean,
         default: false,
       },
+      locked: {
+        type: Boolean,
+        default: false,
+      },
     },
     computed: {
       maxTitleHeight() {
         return 40;
+      },
+    },
+    $trs: {
+      lockedByCoachLabel: {
+        message: 'Locked by your coach',
+        context: 'Shown on a lesson resource card that a coach has locked.',
       },
     },
   };
@@ -123,6 +159,22 @@
 
   .card-content {
     text-decoration: none;
+  }
+
+  .locked-card {
+    cursor: not-allowed;
+    opacity: 0.75;
+  }
+
+  .locked-label {
+    display: flex;
+    align-items: center;
+    margin: 8px 0 0 $margin;
+    font-size: 0.9em;
+
+    /deep/ svg {
+      margin-right: 4px;
+    }
   }
 
   .thumbnail {
